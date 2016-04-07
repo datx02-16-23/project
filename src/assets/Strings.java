3bf5240130c272a77f37ed061bc801203b877055
Richard Sundqvist <Richard Sundqvist>
2016-04-07 21:37:05 +0200

    Added assets.

---
+++ src/assets/Strings.java
@@ -0,0 +1,25 @@
+package assets;
+
+public abstract class Strings {
+
+	public static final String PROJECT_NAME = "Mavser";
+	public static final String PROJECT_SLOGAN = "Från källkod till grafisk representation";
+
+	public static final String DEFAULT_CHANNEL = "mavser_stream";
+	
+		//Keys
+		public static final String KEY_TARGET = "target";
+		public static final String KEY_SOURCE = "source";
+		public static final String KEY_VAR1 = "var1";
+		public static final String KEY_VAR2 = "var2";
+
+		public static final String KEY_IDENTIFIER = "identifier";
+		public static final String KEY_OPERATION = "operation";
+		public static final String KEY_INDEX = "index";
+		public static final String KEY_VALUE = "value";
+		public static final String KEY_SIZE = "size";
+		
+		//Credits
+		public static final String[] DEVELOPER_NAMES = {"Jonathan Skårstedt","Simon Smith","Richard Sundqvist",
+											            "Dennis Jönsson","Johan Gerdin","Ivar Josefsson"};
+}
