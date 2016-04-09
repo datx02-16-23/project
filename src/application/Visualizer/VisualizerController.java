bef3d5050e2660b46fbc89eda135a1b7a828f915
Smith <simonsm@student.chalmers.se>
2016-04-09 11:13:14 +0200

    Add listeners to all buttons
    All buttons do something at least.

--- src/application/Visualizer/VisualizerController.java
+++ src/application/Visualizer/VisualizerController.java
@@ -2,7 +2,10 @@ package application.Visualizer;
 
 import application.model.Model;
 import application.model.iModel;
+import assets.Strings;
+import javafx.event.Event;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import manager.LogStreamManager;
@@ -19,6 +22,10 @@ public class VisualizerController {
     private final LogStreamManager lsm;
     private final iModel model;
 
+    // Controls
+    private boolean isPlaying = false;
+    private int speed = 1;
+
     public VisualizerController(Stage window, iModel model, LogStreamManager lsm) {
         this.window = window;
         this.model = model;
@@ -26,10 +33,57 @@ public class VisualizerController {
     }
 
     /**
-     * Starts playing the AV animation.
+     * Starts playing or pause the AV animation.
      */
-    public void playButtonClicked(){
-        System.out.println("What's up player?");
+    public void playPauseButtonClicked(Event e){
+        if(isPlaying) {
+            ((Button) e.getSource()).setText("Play");
+            System.out.println("Placeholder: Woah! Hold your horses.");
+            isPlaying = false;
+        }
+        else {
+            ((Button) e.getSource()).setText("Pause");
+            System.out.println("Placeholder: What's up player?");
+            isPlaying = true;
+        }
+    }
+
+    /**
+     * Restart the AV animation.
+     */
+    public void restartButtonClicked(){
+        System.out.println("Placeholder: If only there was a button for second chances.");
+    }
+
+    /**
+     * Step the animation forward
+     */
+    public void stepForwardButtonClicked(){
+        System.out.println("Placeholder: So it was you.");
+    }
+
+    /**
+     * Step the animation backward
+     */
+    public void stepBackwardButtonClicked(){
+        System.out.println("Placeholder: aaaand you fell of a cliff");
+    }
+
+    /**
+     * Change the animation speed
+     */
+    public void changeSpeedButtonClicked(Event e){
+        System.out.println("Placeholder: Let's pump it up!");
+        speed = speed*2 % 7; // possible values: 1, 2, 4
+        ((Button) e.getSource()).setText("" + speed + "x");
+    }
+
+    public void aboutProgram(){
+        System.out.print("Placeholder: A project by " );
+        for (String name : Strings.DEVELOPER_NAMES) {
+            System.out.print(name + ", ");
+        }
+        System.out.println();
     }
 
     /**
