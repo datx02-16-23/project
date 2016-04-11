bcb9459fe79e0dc26bd91b76ed59a30c798c7b8e
Richard Sundqvist <Richard Sundqvist>
2016-04-11 15:39:56 +0200

    Work on #45.

--- src/main/java/application/Visualizer/VisualizerModel.java
+++ src/main/java/application/Visualizer/VisualizerModel.java
@@ -17,6 +17,8 @@ import manager.LogStreamManager;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 
 /**
@@ -82,7 +84,27 @@ public class VisualizerModel extends Application {
         window.show();
     }
     
-    public void init(){}
+    private Properties properties;
+    public void init(){
+    	InputStream inputStream;
+		Properties prop = new Properties();
+		String propFileName = "config.properties";
+		
+		inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+ 
+		if (inputStream != null) {
+			prop.load(inputStream);
+			inputStream.close();
+		}
+
+ 
+		// get the property value and print it out
+		String user = prop.getProperty("user");
+		String company1 = prop.getProperty("company1");
+		String company2 = prop.getProperty("company2");
+		String company3 = prop.getProperty("company3");
+			
+    }
 
     public static void main(String[] args) {
         launch(args);
