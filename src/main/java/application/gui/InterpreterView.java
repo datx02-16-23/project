3da16ba673680f24a908d9d165623000854db4c5
Richard Sundqvist <richard.sundqvist@live.se>
2016-04-16 12:53:57 +0200

    Classes to reduce the amount of code in GUI_Controller.

---
+++ src/main/java/application/gui/InterpreterView.java
@@ -0,0 +1,8 @@
+package application.gui;
+
+/**
+ * Controller and model for the Interpeter view.
+ * @author Richard
+ *
+ */
+public class InterpreterView {}
---
+++ src/main/java/application/gui/SettingsView.java
@@ -0,0 +1,8 @@
+package application.gui;
+
+/**
+ * Controller and model for the Settings view.
+ * @author Richard
+ *
+ */
+public class SettingsView {}
