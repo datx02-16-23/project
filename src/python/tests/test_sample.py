a0cd081bb67aba87ec15c0691f3cb1d83210fa52
johan <johan.gerdin@hotmail.com>
2016-04-21 10:54:04 +0200

    remaking package structure

--- src/python/tests/test_sample.py
+++ src/python/tests/test_sample.py
@@ -1,17 +1,17 @@
 from context import sample
-from sample import run
+from sample import annotations
 from os.path import abspath
 
 if __name__ == '__main__':
-	#  sample.run.create_settings Parameters
+	#  sample.annotations.create_settings Parameters
 	#	root_directory, files, variables, main_file, output
-	# sample.run.Variable Constructor
+	# sample.annotations.Variable Constructor
 	# 	name, rawType, attributes=None, abstractType=None
-	settings = run.create_settings(
+	settings = annotations.create_settings(
 		abspath('./'),
 		['main.py'],
-		sample.run.Variable('c','array'),
+		sample.annotations.Variable('c','array'),
 		'main.py',
 		abspath('output.json')
 	)
-	run.run(settings)
+	annotations.run(settings)
